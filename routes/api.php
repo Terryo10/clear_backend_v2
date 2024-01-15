fc56d032f446ca14c6414a235aefc8d5cc81c651
pikigene <pikigene01@gmail.com>
2024-01-16 01:30:49 +0200

    auth routes updates

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Http\Request;
+use App\Http\Controllers\loginController;
+use App\Http\Controllers\registerController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,4 +18,9 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+Route::post('/login', [loginController::class, 'login']);
+Route::post('/register', [registerController::class, 'register']);
+Route::middleware(['auth:sanctum'])->group(function () {
+    Route::post('/logout', [loginController::class, 'logout']);
 });
