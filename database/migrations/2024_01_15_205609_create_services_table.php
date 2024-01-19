ff870b60b53e8bd7bcd9065dc4d0b4828af3cba1
pikigene <pikigene01@gmail.com>
2024-01-20 01:55:29 +0200

    update service database

--- database/migrations/2024_01_15_205609_create_services_table.php
+++ database/migrations/2024_01_15_205609_create_services_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('image');
-            $table->boolean('status')->default(0);
+            $table->string('status')->default('false');
             $table->timestamps();
         });
     }
