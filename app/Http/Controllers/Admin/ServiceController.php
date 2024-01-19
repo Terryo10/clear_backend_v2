c273a9706f89ce5c4aa8d3693791d0c394d21eba
pikigene <pikigene01@gmail.com>
2024-01-19 23:22:22 +0200

    update

--- app/Http/Controllers/Admin/ServiceController.php
+++ app/Http/Controllers/Admin/ServiceController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Models\Service;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\URL;
+use Illuminate\Support\Facades\Validator;
 
 class ServiceController extends Controller
 {
@@ -32,29 +33,36 @@ class ServiceController extends Controller
     public function store(Request $request)
     {
         // Validate the request data
-        $request->validate([
-            'name' => 'required|string|max:255',
-            'image*' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
-            'status' => 'boolean',
-        ]);
-        $images_new = "";
-        if ($file = $request->file('image')) {
-            $var = date_create();
-            $date = date_format($var, 'Ymd');
-            $imageName = $date . '_' . $file->getClientOriginalName();
-            $file->move(public_path() . '/uploads/', $imageName);
-            $url = URL::to("/") . '/uploads/' . $imageName;
-            $images_new = $url;
-        }
-        $input = [
-            "name" => $request->name,
-            "image" => $images_new,
-            "status" => $request->status,
-        ];
-        // Create a new service
-        $service = Service::create($input);
+        $validator = Validator::make(
+            $request->all(),
+            [
+                'name' => 'required|string|max:255',
+                'image*' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+                'status' => 'boolean',
+            ]
+        );
+        if ($validator->fails()) {
+            return response()->json(['status' => 401, 'message' => "validation failes", "errors" => $validator->errors()]);
+        } else {
+            $images_new = "";
+            if ($file = $request->file('image')) {
+                $var = date_create();
+                $date = date_format($var, 'Ymd');
+                $imageName = $date . '_' . $file->getClientOriginalName();
+                $file->move(public_path() . '/uploads/', $imageName);
+                $url = 'public/uploads/' . $imageName;
+                $images_new = $url;
+            }
+            $input = [
+                "name" => $request->name,
+                "image" => $images_new,
+                "status" => $request->status,
+            ];
+            // Create a new service
+            $service = Service::create($input);
 
-        return response()->json(["status" => 200, "message" => "service created successfully"]);
+            return response()->json(["status" => 200, "message" => "service created successfully"]);
+        }
     }
 
     /**
