fcf727ce8619b1d2f55c3a6864c5f28f88d3fc91
Gene Piki <85885450+pikigene01@users.noreply.github.com>
2024-06-30 21:34:58 +0200

    update get latest requests

--- app/Http/Controllers/Admin/ProjectController.php
+++ app/Http/Controllers/Admin/ProjectController.php
@@ -368,7 +368,7 @@ class ProjectController extends Controller
 
     public function getRequests()
     {
-        $requests = Project::paginate(20);
+        $requests = Project::latest()->paginate(20);
         return $this->jsonSuccess(200, 'Request Successful', $requests, 'requests');
         return ProjectResource::collection($requests);
     }
